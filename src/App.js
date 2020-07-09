a73930958a843b5b77de2ddc621f1bab43bc298c
Anjali Gupta <anjalyyy1@gmail.com>
2020-07-09 21:49:06 +0530

    redux store integrated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Routes from './routes';
-import GlobalStyle from './globalStyles';
+import React from "react";
+import Routes from "./routes";
+import GlobalStyle from "./globalStyles";
+
+import { Provider } from "react-redux";
+
+// configure redux store
+import { store } from "./store";
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Provider>
   );
 }
 
